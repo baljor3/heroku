c962e4eb1c54130554c1cbdb65583b7c11246fd3
baljor3 <52223529+baljor3@users.noreply.github.com>
2020-06-21 23:41:17 -0700

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,27 @@
+function getname(person){
+  //console.log('X', person);
+var name = document.getElementById('name');
+name.innerHTML = person;
+/*
+Object.keys(person).forEach(function(key) {
+  console.log(key, person[key]);
+});
+*/
+}
+
+function getheight(person){
+var name = document.getElementById('height');
+height.innerHTML = person;
+}
+function getID(person){
+var name = document.getElementById('height');
+id.innerHTML = person;
+}
+function getSize(person){
+var name = document.getElementById('height');
+size.innerHTML = person;
+}
+function gettype(person){
+var name = document.getElementById('height');
+type.innerHTML = person;
+}
